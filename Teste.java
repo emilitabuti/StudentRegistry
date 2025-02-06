faf7752d8eaea0a7c57815938b9871a29a474405
Emili Tabuti <119452620+emilitabuti@users.noreply.github.com>
2025-02-06 02:33:37 -0300

    Update Teste.java

--- Teste.java
+++ Teste.java
@@ -5,7 +5,7 @@
  * @author Caroline Guimarães Campos, Emili Vieira Tabuti, Gustavo Macedo Costa
  * @version 23/05/2024
  */
-public class Teste implements java.io.Serializable
+public class Cadastro implements java.io.Serializable
 {
    public static void main(String[] args) {
         new InterfaceC();
